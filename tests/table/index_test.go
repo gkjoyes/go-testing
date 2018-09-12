6e37ec5e69131668e6d695a41392f860da4f6056
georgekutty <georgekutty@qburst.com>
2018-09-13 01:50:29 +0530

    Table driven test

---
+++ tests/table/index_test.go
@@ -0,0 +1,29 @@
+package table_test
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	// test cases
+	var tests = []struct {
+		s   string
+		sep string
+		out int
+	}{
+		{"", "", 0},
+		{"", "a", -1},
+		{"fo", "foo", -1},
+		{"foo", "foo", 0},
+		{"ofofof", "f", 1},
+	}
+
+	// run each test cases
+	for _, test := range tests {
+		actual := strings.Index(test.s, test.sep)
+		if actual != test.out {
+			t.Errorf("Index(%q,%q) = %v; want %v", test.s, test.sep, actual, test.out)
+		}
+	}
+}
--- tests/unit/index_test.go
+++ tests/unit/index_test.go
@@ -1,4 +1,4 @@
-package tests
+package unit_test
 
 import (
 	"strings"
